1c5886e6a0c1773f40c792607fa9e81b687e60bc
abyssal-twilight <codeberg@kairi6.anonaddy.com>
2026-03-14 18:12:14 +0100

    Add extra sustituters
    Add nix-community's cache repository as a binary cache extra substituter

--- modules/env/default.nix
+++ modules/env/default.nix
@@ -4,5 +4,6 @@
 	  ./programs.nix
 		./time-zone.nix
 		./unfree.nix
+    ./extra-substituters.nix
 	];
 }
---
+++ modules/env/extra-substituters.nix
@@ -0,0 +1,16 @@
+{ username, ... }:
+
+{
+  nix.settings = {
+    trusted-users = [ username ];
+
+    # nix-community's cache server is included
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
