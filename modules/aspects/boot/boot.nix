0bf4a4ea5a7f63423147f60b156f77789388f798
amanako <github@kairi6.anonaddy.com>
2026-04-24 10:34:36 +0000

    Add a new aspect for plymouth based on nixos wiki

---
+++ modules/aspects/boot/boot.nix
@@ -0,0 +1,31 @@
+{
+  den.aspects.boot = {
+    nixos =
+      { pkgs, ... }:
+      {
+        stylix.targetsToDisable = [ "plymouth" ];
+
+        boot.plymouth = {
+          enable = true;
+          theme = "nixos-bgrt";
+          themePackages = [
+            pkgs.nixos-bgrt-plymouth
+          ];
+        };
+
+        boot = {
+          # Remove verbosity
+          consoleLogLevel = 3;
+          initrd.verbose = false;
+          kernelParams = [
+            "quiet"
+            "udev.log_level=3"
+            "systemd.show_status=auto"
+          ];
+          # Setting to 0 will hide the OS choice for bootloaders
+          # and any keypress will trigger it.
+          loader.timeout = 5;
+        };
+      };
+  };
+}
