beb7229c0d13a134f1b2fbadeef7db4c7925134a
abyssal-twilight <codeberg@kairi6.anonaddy.com>
2026-05-12 14:46:30 +0200

    zen-browser: Fix extensions
    Fix incorrect lambda import and bring in inputs' for preselected system.

--- modules/aspects/browsers/zen-browser/extensions.nix
+++ modules/aspects/browsers/zen-browser/extensions.nix
@@ -1,40 +1,44 @@
 {
-  zen-browser.extensions = {
-    inputs',
-    lib,
-    ...
-  }: {
-    homeManager.programs.zen-browser.profiles."*".extensions = {
-      packages = with inputs'.firefox-addons.packages; [
-        darkreader
-        proton-pass
-        vimium-c
-        ublock-origin
-        addy_io
-      ];
+  zen-browser.extensions = {user, ...}: {
+    homeManager = {
+      inputs',
+      lib,
+      ...
+    }: {
+      programs.zen-browser.profiles."${user.userName}".extensions = {
+        force = true;
 
-      settings = {
-        "uBlock0@raymondhill.net".settings = {
-          userSettings = {
-            uiTheme = "dark";
-            cloudStorageEnabled = lib.mkForce false;
+        packages = with inputs'.firefox-addons.packages; [
+          darkreader
+          proton-pass
+          vimium-c
+          ublock-origin
+          addy_io
+        ];
+
+        settings = {
+          "uBlock0@raymondhill.net".settings = {
+            userSettings = {
+              uiTheme = "dark";
+              cloudStorageEnabled = lib.mkForce false;
+            };
+            selectedFilterLists = [
+              "ublock-filters"
+              "ublock-badware"
+              "ublock-privacy"
+              "ublock-quick-fixes"
+              "plowe-0"
+              "easyprivacy"
+              "fanboy-cookiemonster"
+              "ublock-cookies-easylist"
+              "fanboy-social"
+              "ublock-annoyances"
+            ];
           };
-          selectedFilterLists = [
-            "ublock-filters"
-            "ublock-badware"
-            "ublock-privacy"
-            "ublock-quick-fixes"
-            "plowe-0"
-            "easyprivacy"
-            "fanboy-cookiemonster"
-            "ublock-cookies-easylist"
-            "fanboy-social"
-            "ublock-annoyances"
-          ];
-        };
 
-        "addon@darkreader.org".settings = {
-          syncSettings = true;
+          "addon@darkreader.org".settings = {
+            syncSettings = true;
+          };
         };
       };
     };
