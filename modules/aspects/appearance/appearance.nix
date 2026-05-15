d2e614a8607a4a2fe9659f7f82998856cff68f96
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-05-15 21:53:08 +0200

    Appearance: Fix wallpaper daemon
    Add ExecStartPost to awww-daemon and simplify parts of awww-random

--- modules/aspects/appearance/appearance.nix
+++ modules/aspects/appearance/appearance.nix
@@ -1,82 +1,76 @@
 {inputs, ...}: {
-  flake-file.inputs = {
-    wallpapers = {
-      url = "git+https://codeberg.org/voidptrx/wallpapers";
-      flake = false;
-    };
+  flake-file.inputs.wallpapers = {
+    url = "git+https://codeberg.org/voidptrx/wallpapers";
+    flake = false;
   };
 
-  den.aspects.appearance.homeManager = {
-    pkgs,
-    lib,
-    ...
-  }: let
-    wallpapersPath = inputs.wallpapers.outPath;
-    awwwPkg = pkgs.awww;
-    find = lib.getExe' pkgs.findutils "find";
-    shuf = lib.getExe' pkgs.coreutils "shuf";
+  den.aspects.appearance = {
+    persysUser.directories = [".cache/awww"];
 
-    wallpaperScript = pkgs.writeShellScriptBin "awww-random" ''
-      DIR="${wallpapersPath}"
-      img=$( ${find} "$DIR" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.gif" -o -iname "*.webp" -o -iname "*.bmp" \) | ${shuf} -n 1)
-      if [ -n "$img" ]; then
-        ${lib.getExe awwwPkg} img --transition-fps 144 --transition-type wave --transition-angle 225 --resize=fit "$img"
-      fi
-    '';
-  in {
-    home.packages = [
-      awwwPkg
-      wallpaperScript
-    ];
+    homeManager = {
+      pkgs,
+      lib,
+      ...
+    }: let
+      wallpapersPath = inputs.wallpapers.outPath;
 
-    systemd.user = {
-      services = {
-        # Service to start the daemon
-        awww-daemon = {
+      awwwExe = lib.getExe' pkgs.awww "awww";
+      awwwDaemon = lib.getExe' pkgs.awww "awww-daemon";
+      systemctl = lib.getExe' pkgs.systemdMinimal "systemctl";
+      find = lib.getExe' pkgs.findutils "find";
+      shuf = lib.getExe' pkgs.coreutils "shuf";
+
+      wallpaperScript = pkgs.writeShellScriptBin "awww-random" ''
+        DIR="${wallpapersPath}"
+        img=$( ${find} "$DIR" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.gif" -o -iname "*.webp" -o -iname "*.bmp" \) | ${shuf} -n 1)
+        if [ -n "$img" ]; then
+          ${awwwExe} img --transition-fps 144 --transition-type wave --transition-angle 225 --resize=fit "$img"
+        fi
+      '';
+    in {
+      systemd.user = {
+        services = {
+          # Service to start the daemon
+          awww-daemon = {
+            Unit = {
+              Description = "awww Wallpaper daemon";
+              After = ["graphical-session.target"];
+              Wants = ["awww-random.timer"];
+            };
+
+            Service = {
+              ExecStart = "${awwwDaemon}";
+              ExecStartPost = "${systemctl} --user start awww-random.service";
+              Restart = "on-failure";
+              RestartSec = 1;
+            };
+
+            Install.WantedBy = ["graphical-session.target"];
+          };
+
+          # Service to rotate wallpapers
+          awww-random = {
+            Unit.Description = "Wallpaper rotator";
+
+            Service = {
+              ExecStart = "${lib.getExe wallpaperScript}";
+              Restart = "on-failure";
+              RestartSec = 2;
+
+              Type = "oneshot";
+            };
+          };
+        };
+
+        # Control unit - currently on :00 and :30 of every hour
+        timers.awww-random = {
           Unit = {
-            Description = "awww Wallpaper daemon";
-            After = ["graphical-session.target"];
-            Wants = ["awww-random.timer"];
+            Description = "Change wallpaper every 30 minutes";
+            BindsTo = ["awww-daemon.service"];
+            Wants = ["awww-daemon.service"];
           };
 
-          Service = {
-            ExecStart = "${lib.getExe awwwPkg}";
-            Restart = "on-failure";
-            RestartSec = 1;
-          };
-
-          Install = {
-            WantedBy = ["graphical-session.target"];
-          };
-        };
-
-        # Service to rotate wallpapers
-        awww-random = {
-          Unit = {
-            Description = "Wallpaper rotator";
-          };
-
-          Service = {
-            ExecStart = "${lib.getExe wallpaperScript}";
-            Restart = "on-failure";
-            RestartSec = 2;
-
-            Type = "oneshot";
-          };
-        };
-      };
-
-      # Control unit - currently on every 30 minutes
-      timers.awww-random = {
-        Unit = {
-          Description = "Change wallpaper every 30 minutes";
-          BindsTo = ["awww-daemon.service"];
-          Wants = ["awww-random.service"];
-        };
-
-        Timer = {
-          OnUnitActiveSec = "30min";
-          AccuracySec = "1s";
+          Timer.OnUnitActiveSec = "30min";
         };
       };
     };
