c315dd7421cf5c6c52a4c36eeb9fa9bc85250c6d
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-05-17 15:50:31 +0200

    Fix: move git aspect from den.default to natural den.schema.user
    Use knowledge obtained from error and trial in commit 526d26b8
    to move from unnatural den.default to more logical den.schema.user
    for user concern of configuring git.

--- modules/den/schema/user/git.nix
+++ modules/den/schema/user/git.nix
@@ -1,33 +1,36 @@
-{den, ...}: {
+{
+  den,
+  lib,
+  ...
+}: {
   den = {
-    schema.user = {lib, ...}: {
-      options.git = lib.mkOption {
-        type = lib.types.submodule {
-          options = {
-            username = lib.mkOption {
-              type = lib.types.str;
-              example = "git";
-              default = "";
-              description = "Username to use for git. Will be passed to `homeManager.programs.git.settings.user.username`.";
-            };
-            email = lib.mkOption {
-              type = lib.types.str;
-              example = "git@git.com";
-              default = "";
-              description = "Email to use for git. Will be passed to `homeManager.programs.git.settings.user.email`.";
-            };
-            signingKey = lib.mkOption {
-              type = lib.types.str;
-              example = "A1B2C3D4E5F6G7H8";
-              default = "";
-              description = "Signing key consisting of 16 alphanumeric characters to use for commits. Will be passed to `homeManager.programs.git.signing.key`.";
-            };
+    schema.user.options.git = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          username = lib.mkOption {
+            type = lib.types.str;
+            example = "git";
+            default = "";
+            description = "Username to use for git. Will be passed to `homeManager.programs.git.settings.user.username`.";
+          };
+          email = lib.mkOption {
+            type = lib.types.str;
+            example = "git@git.com";
+            default = "";
+            description = "Email to use for git. Will be passed to `homeManager.programs.git.settings.user.email`.";
+          };
+          signingKey = lib.mkOption {
+            type = lib.types.str;
+            example = "A1B2C3D4E5F6G7H8";
+            # Follow upstream option default
+            default = null;
+            description = "Signing key consisting of 16 alphanumeric characters to use for commits. Will be passed to `homeManager.programs.git.signing.key`.";
           };
         };
       };
     };
 
-    default.includes = [den.aspects.git];
+    schema.user.includes = [den.aspects.git];
 
     aspects.git.homeManager = {user, ...}: {
       programs.git = {
