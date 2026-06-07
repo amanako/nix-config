f9b4af1c262ee9a0c7b257bd9f067d6b2a68d8ab
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-06-07 14:20:54 +0200

    Fcitx5: Fix incorrect aspect import & enable stylix
    Fix bug where new users would cause lambda issues and add more explicit
    include of aspect instead of importing default config for all users via
    den.schema.config. Also enable stylix for the aspect after since it is
    now declarative.

---
+++ modules/aspects/everyday/utility/fcitx5.nix
@@ -0,0 +1,49 @@
+{
+  den,
+  lib,
+  ...
+}: let
+  fcitx5Class = {user, ...}:
+    den.batteries.forward {
+      each = lib.singleton true;
+      fromClass = _: "fcitx5";
+      intoClass = _: "homeManager";
+      intoPath = _: ["i18n" "inputMethod" "fcitx5"];
+      fromAspect = _: user.aspect;
+      guard = _: user.aspect ? "fcitx5";
+    };
+in {
+  den.aspects.utility.fcitx5 = {
+    includes = [fcitx5Class];
+
+    niriSpawnAtStartup = {
+      command = ["fcitx5"];
+    };
+
+    stylixHome.targets."fcitx5".enable = true;
+
+    homeManager = {
+      home.sessionVariables = {
+        GTK_IM_MODULE = "fcitx5";
+        QT_IM_MODULE = "fcitx5";
+      };
+
+      i18n.inputMethod = {
+        enable = true;
+        type = "fcitx5";
+
+        # Suppress some warnings
+        fcitx5.waylandFrontend = true;
+      };
+    };
+  };
+
+  den.schema.user.includes = [
+    (
+      {user}:
+        if user.aspect ? fcitx5
+        then den.aspects.utility.fcitx5
+        else {}
+    )
+  ];
+}
--- modules/users/lunar-scar/aspect/fcitx5-config.nix
+++ modules/users/lunar-scar/aspect/fcitx5-config.nix
@@ -1,9 +1,7 @@
 {
-  # Aspect should respect fcitx5 forwarging battery and accept user as lambda parameter.
-  # For naming of direct aspect, anything other that fcitx5 should suffice and help avoid confusion.
-  den.aspects.lunar-scar.fcitx5-config = {user, ...}: {
+  den.aspects.lunar-scar = {
     # Addons require access to pkgs instance but fcitx5 is a static class.
-    # Therefore, define a homeManager(or nixos) class to append addons
+    # Therefore, define a homeManager class to append addons
     homeManager = {pkgs, ...}: {
       i18n.inputMethod.fcitx5.addons = with pkgs; [
         # Basic GUI for configuration
@@ -13,74 +11,77 @@
       ];
     };
 
-    fcitx5.settings = {
-      inputMethod = {
-        GroupOrder = {
-          "0" = "Default";
-          "1" = "JP";
-          "2" = "SR - Latin";
-          "3" = "SR - Cyrilic";
+    # Reference: https://wiki.nixos.org/wiki/Fcitx5#Configuration
+    fcitx5 = {
+      settings = {
+        inputMethod = {
+          GroupOrder = {
+            "0" = "Default";
+            "1" = "JP";
+            "2" = "SR - Latin";
+            "3" = "SR - Cyrilic";
+          };
+
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+
+          "Groups/1" = {
+            Name = "JP";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/1/Items/0".Name = "mozc";
+
+          "Groups/2" = {
+            Name = "SR - Latin";
+            "Default Layout" = "rs-latinalternatequotes";
+            DefaultIM = "keyboard-rs-latinalternatequotes";
+          };
+          "Groups/2/Items/0".Name = "keyboard-rs-latinalternatequotes";
+
+          "Groups/3" = {
+            Name = "SR - Cyrilic";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-rs-alternatequotes";
+          };
+          "Groups/3/Items/0".Name = "keyboard-rs-alternatequotes";
         };
 
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "us";
-          DefaultIM = "keyboard-us";
-        };
-        "Groups/0/Items/0".Name = "keyboard-us";
+        globalOptions = {
+          Behavior = {
+            ActiveByDefault = true;
+            resetStateWhenFocusIn = "No";
+            ShareInputState = "All";
+            PreeditEnabledByDefault = true;
+            ShowInputMethodInformation = true;
+            showInputMethodInformationWhenFocusIn = false;
+            CompactInputMethodInformation = true;
+            ShowFirstInputMethodInformation = true;
+            DefaultPageSize = 5;
+            PreloadInputMethod = true;
+            AllowInputMethodForPassword = false;
+            ShowPreeditForPassword = false;
+            AutoSavePeriod = 30;
+          };
 
-        "Groups/1" = {
-          Name = "JP";
-          "Default Layout" = "us";
-          DefaultIM = "mozc";
-        };
-        "Groups/1/Items/0".Name = "mozc";
+          # Hotkey settings
+          Hotkey = {
+            EnumerateWithTriggerKeys = true;
+            EnumerateSkipFirst = true;
+            ModifierOnlyKeyTimeout = 252;
+          };
 
-        "Groups/2" = {
-          Name = "SR - Latin";
-          "Default Layout" = "rs-latinalternatequotes";
-          DefaultIM = "keyboard-rs-latinalternatequotes";
+          "Hotkey/TriggerKeys"."0" = "Control+space";
+          "Hotkey/AltTriggerKeys"."0" = "Shift_L";
+          "Hotkey/EnumerateGroupForwardKeys"."0" = "Alt+space";
+          "Hotkey/EnumerateGroupBackwardKeys"."0" = "Alt+Shift+space";
+          "Hotkey/PrevCandidate"."0" = "Shift+Tab";
+          "Hotkey/NextCandidate"."0" = "Tab";
         };
-        "Groups/2/Items/0".Name = "keyboard-rs-latinalternatequotes";
-
-        "Groups/3" = {
-          Name = "SR - Cyrilic";
-          "Default Layout" = "us";
-          DefaultIM = "keyboard-rs-alternatequotes";
-        };
-        "Groups/3/Items/0".Name = "keyboard-rs-alternatequotes";
-      };
-
-      globalOptions = {
-        Behavior = {
-          ActiveByDefault = true;
-          resetStateWhenFocusIn = "No";
-          ShareInputState = "All";
-          PreeditEnabledByDefault = true;
-          ShowInputMethodInformation = true;
-          showInputMethodInformationWhenFocusIn = false;
-          CompactInputMethodInformation = true;
-          ShowFirstInputMethodInformation = true;
-          DefaultPageSize = 5;
-          PreloadInputMethod = true;
-          AllowInputMethodForPassword = false;
-          ShowPreeditForPassword = false;
-          AutoSavePeriod = 30;
-        };
-
-        # Hotkey settings
-        Hotkey = {
-          EnumerateWithTriggerKeys = true;
-          EnumerateSkipFirst = true;
-          ModifierOnlyKeyTimeout = 252;
-        };
-
-        "Hotkey/TriggerKeys"."0" = "Control+space";
-        "Hotkey/AltTriggerKeys"."0" = "Shift_L";
-        "Hotkey/EnumerateGroupForwardKeys"."0" = "Alt+space";
-        "Hotkey/EnumerateGroupBackwardKeys"."0" = "Alt+Shift+space";
-        "Hotkey/PrevCandidate"."0" = "Shift+Tab";
-        "Hotkey/NextCandidate"."0" = "Tab";
       };
     };
   };
