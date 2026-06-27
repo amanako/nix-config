7bfa1f6062d09f5c816f47da58e90bdc83ded3d6
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-06-27 18:08:36 +0200

    Niri: fix keyboard backlight
    Apply necessary fixes to resolve faulty keyboard backlight script for niri.
    Generalize the aspect to accept device instead of always passing one.

--- modules/aspects/everyday/compositors/niri/settings/general/keyboard-backlight.nix
+++ modules/aspects/everyday/compositors/niri/settings/general/keyboard-backlight.nix
@@ -10,7 +10,6 @@
         programs.niri.settings.binds = let
           keyboardLightScriptPath = let
             brightnessCmd = pkgs.brightnessctl |> lib.getExe;
-            inherit (host.keyboardLightScript) device;
           in
             pkgs.writeTextFile rec {
               name = "keyboard-light";
@@ -18,31 +17,32 @@
               destination = "/bin/${name}";
               text = ''
                 #!${pkgs.nushell |> lib.getExe}
-                def update_brightness [command: string, percent = 0.10] {
-                    let max_brightness = ^brightnessctl --device ${device} max | into int;
-                    let step = [(($max_brightness | into float) * $percent | into int), 1] | math max;
-                    let curr = ^${brightnessCmd} --device ${device} get | into int
+                def find_new_brightness [device: string, command: string, percent: float] {
+                    let max_brightness = ^${brightnessCmd} --device $device max | into int
+                    let step = [(($max_brightness | into float) * $percent | into int), 1] | math max
+                    let curr = ^${brightnessCmd} --device $device get | into int
 
                     match $command {
                         "increase" => ($curr + $step)
                         # Ensure command doesn't fail for negative values
                         "decrease" => ([ 0, ($curr - $step) ] | math max)
                         _ => {
-                            print "Usage: $scriptname <increase|decrease>"
+                            print "Usage: ${name} <device> <increase|decrease> [percent]"
                             exit 1
                         }
                     }
                 }
 
-                def main [command: string, percent = 0.10] {
-                  update_brightness $command $percent | ^brightnessctl --device "asus::kbd_backlight" set $in
+                def main [device: string, command: string, percent = 0.10] {
+                  find_new_brightness $device $command $percent | ^${brightnessCmd} --device $device set $in
                 }
               '';
             };
           sh = cmd: {action.spawn-sh = cmd;};
+          inherit (host.keyboardLightScript) device;
         in {
-          "Alt+Up" = sh "${keyboardLightScriptPath |> lib.getExe} increase";
-          "Alt+Down" = sh "${keyboardLightScriptPath |> lib.getExe} decrease";
+          "Alt+Up" = sh "${keyboardLightScriptPath |> lib.getExe} ${device} increase";
+          "Alt+Down" = sh "${keyboardLightScriptPath |> lib.getExe} ${device} decrease";
         };
       };
   };
