fb1170fccc24a7b9fa54f8b2f9cbe6e5166f7911
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-07-19 12:34:16 +0200

    fixup(vix): remove llm-agents & move to vix
    - As a setup for future possible coding agents, remove generic
      llm-agents aspect
    - Move flake-file input declaration to vix aspect

--- modules/den/aspects/dev/terminal/vix.nix
+++ modules/den/aspects/dev/terminal/vix.nix
@@ -1,4 +1,13 @@
 {lib, ...}: {
+  flake-file = {
+    inputs.llm-agents.url = "github:numtide/llm-agents.nix";
+
+    nixConfig = {
+      extra-substituters = ["https://cache.numtide.com"];
+      extra-trusted-public-keys = ["niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="];
+    };
+  };
+
   den.aspects.dev.terminal.vix = {
     description = "Sleek, Fast and Token Efficient AI Coding Agent";
 
