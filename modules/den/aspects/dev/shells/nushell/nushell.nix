1ea4fb4bff1a8fe8ef95be0e709216552a6a4903
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-07-23 12:13:05 +0200

    fix(nushell): correct mistaken configuration options
    - persist correct FILE instead of incorrect FOLDER (~/.config/nushell/history.txt)
    - explicitly source gpg and NH_FLAKE variables since they dont work as
      expected in nushell

--- modules/den/aspects/dev/shells/nushell/nushell.nix
+++ modules/den/aspects/dev/shells/nushell/nushell.nix
@@ -1,14 +1,21 @@
 {
   den.aspects.dev.shells.nushell = {
-    persistUser.directories = [
-      ".config/nu/history.txt"
+    persistUser.files = [
+      ".config/nushell/history.txt"
     ];
 
-    hm.programs = {
-      nushell.enable = true;
-      direnv.enableNushellIntegration = true;
-      eza.enableNushellIntegration = true;
-      yazi.enableNushellIntegration = true;
+    hm = {config, ...}: {
+      programs.nushell = {
+        enable = true;
+        extraConfig = ''
+          $env.GPG_TTY = (tty | str trim)
+
+          # nushell doesn't inherit home.sessionVariables like bash/fish do,
+          # so env vars set by home-manager modules must be redeclared here.
+          $env.GNUPGHOME = '${config.programs.gpg.homedir}'
+          $env.NH_FLAKE = '${config.programs.nh.flake}'
+        '';
+      };
     };
   };
 }
