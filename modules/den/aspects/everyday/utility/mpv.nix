d66e0821c92f217a277fba7a793c76c5bf046be8
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-07-29 11:11:50 +0200

    feat(mpv): add scripts support
    - Add a couple of useful scripts to mpv home package to extend
      functionality
    - Add additional folder to preserve @eisa01 mpv scripts-related content

--- modules/den/aspects/everyday/utility/mpv.nix
+++ modules/den/aspects/everyday/utility/mpv.nix
@@ -2,11 +2,42 @@
   den.aspects.everyday.utility.mpv = {
     description = "A free, open-source, and cross-platform media player.";
 
-    hm.programs.mpv = {
-      enable = true;
-      bindings = {
-        "h" = "seek -5";
-        "l" = "seek 5";
+    persistUser.files = let
+      basePath = ".config/mpv/";
+    in
+      [
+        "mpvBookmark.log"
+        "mpvHistory.log"
+      ]
+      |> map (path: basePath + path);
+
+    hm = {pkgs, ...}: {
+      programs.mpv = {
+        enable = true;
+        package = pkgs.mpv.override {
+          scripts = with pkgs.mpvScripts; [
+            # Integrate with mpris
+            mpris
+            # Updates to OSC
+            modernz
+            # Skip sponsors
+            sponsorblock
+
+            eisa01.smartskip
+            eisa01.undoredo
+            eisa01.simplehistory
+            eisa01.simplebookmark
+          ];
+        };
+
+        bindings = {
+          "h" = "seek -5";
+          "l" = "seek 5";
+        };
+        config = {
+          osc = "no";
+          border = "no";
+        };
       };
     };
   };
