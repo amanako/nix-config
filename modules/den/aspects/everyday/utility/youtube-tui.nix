012df14b2d973a5e5a8f31fe12f2e4a3622b84e9
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-07-29 11:11:50 +0200

    fix(youtube-tui): change persistUser options
    - preserve whole .local/share/youtube-tui folder instead of splitting due
      to strange issues when syncing feeds, no cached thumbnails and
    incorrect metadata

--- modules/den/aspects/everyday/utility/youtube-tui.nix
+++ modules/den/aspects/everyday/utility/youtube-tui.nix
@@ -4,17 +4,9 @@
       Aesthetically pleasing YouTube TUI written in Rust.
     '';
 
-    persistUser = {
-      directories = [
-        ".local/share/youtube-tui/channels"
-        ".local/share/youtube-tui/info"
-      ];
-
-      files = [
-        ".local/share/youtube-tui/subscriptions.json"
-        ".local/share/youtube-tui/watch_history.json"
-      ];
-    };
+    persistUser.directories = [
+      ".local/share/youtube-tui"
+    ];
 
     desktopEntries = {
       "youtube-tui" = {
