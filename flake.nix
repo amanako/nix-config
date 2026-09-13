bf06aeab042f13c6e5426d0d661c4b80cce125da
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-09-13 23:39:57 +0200

    fix(lockfile): regenerate import-tree input
    - Correct owner by running just fw to fix repo owner from vic -> denful
      and align with internal implementation

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
     flatpaks.url = "github:in-a-dil-emma/declarative-flatpak/latest";
     home-manager.url = "github:nix-community/home-manager";
     impermanence.url = "github:nix-community/impermanence";
-    import-tree.url = "github:vic/import-tree";
+    import-tree.url = "github:denful/import-tree";
     llm-agents.url = "github:numtide/llm-agents.nix";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     niri.url = "github:sodiboo/niri-flake/very-refactor";
