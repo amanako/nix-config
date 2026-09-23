7d4a0e486600da3ce722bd4e24ea28705313a8ea
abyssal-twilight <abyssal-twilight@noreply.codeberg.org>
2026-09-24 00:10:32 +0200

    remove(default-shell): drop welcoming message
    - Remove noisy message echoing each time when entering shell

--- modules/shells/default-shell.nix
+++ modules/shells/default-shell.nix
@@ -27,7 +27,6 @@
         ];
 
         shellHook = ''
-          echo "Welcome to my nix-config repo!"
           export PRE_COMMIT_HOME=$HOME/.cache/pre-commit
           # For pre commit to use pure nix packages instead of downloading
           export PRE_COMMIT_USE_NIX=1
